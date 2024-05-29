1989342d6373428b6e1b2aed714afff22a8e28ed
AriellyGarcia <ariellygarcia25@gmail.com>
2024-05-29 19:05:00 -0300

    Boolean Undefined e Null

---
+++ 2 - Tipos Primitivos/2.3 - Boolean/index.js
@@ -0,0 +1,15 @@
+/*
+    o tipo boolean é a lógica de algoritmo,
+    retorna falso para os casos falsos e true para os verdadeiros
+    utilizamos para comparar valores e validar dados
+*/
+console.log(typeof false); //retorna o tipo do dado, boolean
+
+console.log(false);
+console.log(true);
+
+console.log("*******");
+
+console.log(1 === 2); // 1 é igual a 2? false
+console.log(2 === 2); // 2 é igual a 2? true
+
---
+++ 2 - Tipos Primitivos/2.4 - Undefined e Null/index.js
@@ -0,0 +1,11 @@
+console.log(undefined); //indefinido, coisas que não foram definidas
+console.log(null); //nulo, vazio
+
+////////////////////////////// retorna undefined, a variavel resposta não está definida
+let resposta;
+console.log(resposta);
+
+////////////////////////////// nulo = algo que veio nulo
+
+// retorna false pois null e undefined são coisas diferentes
+console.log(null === undefined);
