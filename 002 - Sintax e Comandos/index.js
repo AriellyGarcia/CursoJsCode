acdcc52be2aba141022e41db340c8e74a2355d71
AriellyGarcia <ariellygarcia25@gmail.com>
2024-05-29 15:48:50 -0300

    Syntax e Comandosclear

---
+++ 002 - Sintax e Comandos/index.js
@@ -0,0 +1,12 @@
+console.log("Olá Mundo!");
+/*SYNTAX
+é a forma que o computador/linguagem pede que escrevemos os comandos
+ERRADO - ConsolE.Log("Olá Mundo!");
+Pois o javascript pede que seja minusculo -> console.log("Olá Mundo!");
+*/
+
+//comentário de linha
+/* 
+blocos de comentários
+*/
+
